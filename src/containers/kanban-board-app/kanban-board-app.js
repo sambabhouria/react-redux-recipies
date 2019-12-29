109c16e50999fb95e7327d9aa77d253739c63911
sambabhouria <sambabhouria@gmail.com>
2019-12-29 21:57:01 +0100

    adding json data

--- src/containers/kanban-board-app/kanban-board-app.js
+++ src/containers/kanban-board-app/kanban-board-app.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
 import KanbanBoard from '../../components/kanban-board-app/kanban-board';
 
-import constants_1 from '../../components/kanban-board-app/constants';
-
-
-// If you're running the server locally, the URL will be, by default, localhost:3000
-//https://jsonplaceholder.typicode.com/
-// Also, the local server doesn't need an authorization header.
-// const API_URL = 'localhost:3000';
-// const API_HEADERS = {
-// 'Content-Type': 'application/json',
-// Authorization: 'any-string-you-like'// The Authorization is not needed for local server
-// };
+const API_HEADERS = {
+'Content-Type': 'application/json',
+ 'Accept': 'application/json'
+};
 
 
 class KanbanBoardContainer extends Component {
@@ -21,33 +14,20 @@ class KanbanBoardContainer extends Component {
       cards: [],
     };
   }
-
    componentDidMount() {
-      //  https://jsonplaceholder.typicode.com/
-       fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-        .then(json => console.log(json))
-    
-          // fetch('../../components/kanban-board-app/data/')
-          // .then(response => {
-          //     if (!response.ok) {
-          //         throw new Error("HTTP error " + response.status);
-          //     }
-          //     return response.json();
-          // })
-          // .then(json => {
-          //     this.users = json;
-          //     //console.log(this.users);
-          // })
-          // .catch(function () {
-          //     this.dataError = true;
-          // })
-      
-    }
 
-  render() {
+     fetch(`card.json`, {headers: API_HEADERS})
+        .then((response) => response.json())
+        .then((responseData) => {
+           this.setState({cards: responseData});
+        })
+        .catch((error) => {
+          console.log('Error fetching and parsing data', error);
+      });
     
-    return <KanbanBoard cards={this.props.cards} />;
+    }
+  render() {
+        return <KanbanBoard cards={this.state.cards} />
   }
 }
 export default KanbanBoardContainer;
